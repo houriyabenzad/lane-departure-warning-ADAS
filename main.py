dd930ab057173d434b01c5be43f5ca0708127678
Houriya BEN ZAD <rpc@MacBook-Pro.local>
2026-06-23 13:24:29 +0100

    Initial project setup

---
+++ main.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+
+print("Lane Departure Warning ADAS project started successfully.")
+print("OpenCV version:", cv2.__version__)
+print("NumPy version:", np.__version__)
